48782a2a9b2f3e581196e1e359e3fc8db24bd785
0x0scion <scionfinance@protonmail.com>
2022-11-30 16:38:35 -0500

    epoch aggregator vault

---
+++ src/vaults/ERC4626/SectorBaseEpoch.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.16;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC4626, FixedPointMathLib, SafeERC20 } from "./ERC4626.sol";
+import { BatchedWithdrawEpoch } from "./BatchedWithdrawEpoch.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { EAction } from "../../interfaces/Structs.sol";
+import { VaultType } from "../../interfaces/Structs.sol";
+
+// import "hardhat/console.sol";
+
+abstract contract SectorBaseEpoch is BatchedWithdrawEpoch {
+	using FixedPointMathLib for uint256;
+	using SafeERC20 for ERC20;
+
+	VaultType public constant vaultType = VaultType.Aggregator;
+
+	uint256 public totalChildHoldings;
+	uint256 public floatAmnt; // amount of underlying tracked in vault
+	uint256 public lastHarvestTimestamp;
+
+	constructor() {
+		lastEpochTimestamp = block.timestamp;
+		lastHarvestTimestamp = block.timestamp;
+	}
+
+	function processRedeem() public onlyRole(MANAGER) {
+		// ensure we have the funds to cover withdrawals
+		uint256 pendingWithdraw = convertToAssets(requestedRedeem);
+		if (floatAmnt < pendingWithdraw) revert NotEnoughtFloat();
+		_processRedeem();
+	}
+
+	function _harvest(uint256 currentChildHoldings) internal {
+		uint256 tvl = currentChildHoldings + floatAmnt;
+		uint256 _totalSupply = totalSupply(); // Saves an extra SLOAD if totalSupply is non-zero.
+
+		uint256 profit = currentChildHoldings > totalChildHoldings
+			? currentChildHoldings - totalChildHoldings
+			: 0;
+
+		uint256 timestamp = block.timestamp;
+
+		// totalChildHoldings need to be updated before fees computation
+		totalChildHoldings = currentChildHoldings;
+
+		// PROCESS VAULT FEES
+		uint256 _performanceFee = profit == 0 ? 0 : (profit * performanceFee) / 1e18;
+		uint256 _managementFee = managementFee == 0
+			? 0
+			: (managementFee * tvl * (timestamp - lastHarvestTimestamp)) / 1e18 / 365 days;
+
+		uint256 totalFees = _performanceFee + _managementFee;
+		uint256 feeShares;
+
+		if (totalFees > 0) {
+			// this results in more accurate accounting considering dilution
+			feeShares = totalFees.mulDivDown(_totalSupply, tvl - totalFees);
+			_mint(treasury, feeShares);
+		}
+
+		emit Harvest(treasury, profit, _performanceFee, _managementFee, feeShares, tvl);
+
+		lastHarvestTimestamp = timestamp;
+	}
+
+	/// @notice this method allows an arbitrary method to be called by the owner in case of emergency
+	/// owner must be a timelock contract in order to allow users to redeem funds in case they suspect
+	/// this action to be malicious
+	function emergencyAction(EAction[] calldata actions) public onlyOwner {
+		uint256 l = actions.length;
+		for (uint256 i = 0; i < l; i++) {
+			address target = actions[i].target;
+			bytes memory data = actions[i].data;
+			(bool success, ) = target.call{ value: actions[i].value }(data);
+			require(success, "emergencyAction failed");
+			emit EmergencyAction(target, data);
+		}
+	}
+
+	function _checkSlippage(
+		uint256 expectedValue,
+		uint256 actualValue,
+		uint256 maxDelta
+	) internal pure {
+		uint256 delta = expectedValue > actualValue
+			? expectedValue - actualValue
+			: actualValue - expectedValue;
+		if (delta > maxDelta) revert SlippageExceeded();
+	}
+
+	function totalAssets() public view virtual override returns (uint256) {
+		return floatAmnt + totalChildHoldings;
+	}
+
+	/// INTERFACE UTILS
+
+	/// @dev returns a cached value used for withdrawals
+	function underlyingBalance(address user) public view returns (uint256) {
+		uint256 shares = balanceOf(user);
+		return convertToAssets(shares);
+	}
+
+	function getUpdatedUnderlyingBalance(address user) public view returns (uint256) {
+		uint256 shares = balanceOf(user);
+		return convertToAssets(shares);
+	}
+
+	function underlyingDecimals() public view returns (uint8) {
+		return asset.decimals();
+	}
+
+	function underlying() public view returns (address) {
+		return address(asset);
+	}
+
+	/// OVERRIDES
+
+	function afterDeposit(uint256 assets, uint256) internal override {
+		floatAmnt += assets;
+	}
+
+	function beforeWithdraw(uint256 assets, uint256) internal override {
+		// this check prevents withdrawing more underlying from the vault then
+		// what we need to keep to honor withdrawals
+		uint256 pendingWithdraw = convertToAssets(pendingRedeem);
+		if (floatAmnt < assets + pendingWithdraw) revert NotEnoughtFloat();
+		floatAmnt -= assets;
+	}
+
+	event RegisterDeposit(uint256 total);
+	event EmergencyWithdraw(address vault, address client, uint256 shares);
+	event EmergencyAction(address target, bytes callData);
+	event Harvest(
+		address indexed treasury,
+		uint256 underlyingProfit,
+		uint256 performanceFee,
+		uint256 managementFee,
+		uint256 sharesFees,
+		uint256 tvl
+	);
+
+	error RecentHarvest();
+	error MaxRedeemNotZero();
+	error NotEnoughtFloat();
+	error WrongUnderlying();
+	error SlippageExceeded();
+	error StrategyExists();
+	error StrategyNotFound();
+	error MissingDepositValue();
+	error EmergencyRedeemEnabled();
+}
---
+++ src/vaults/sectorVaults/AggregatorEpochVault.sol
@@ -0,0 +1,233 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.16;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC4626, FixedPointMathLib, SafeERC20, Fees, FeeConfig, Auth, AuthConfig } from "../ERC4626/ERC4626.sol";
+import { IVaultStrategy } from "../../interfaces/IVaultStrategy.sol";
+import { BatchedWithdraw } from "../ERC4626/BatchedWithdraw.sol";
+import { SectorBaseEpoch } from "../ERC4626/SectorBaseEpoch.sol";
+import { VaultType } from "../../interfaces/Structs.sol";
+
+// import "hardhat/console.sol";
+// TODO native asset deposit + flow
+
+struct RedeemParams {
+	IVaultStrategy strategy;
+	VaultType vaultType;
+	uint256 shares;
+	uint256 minTokenOut;
+}
+
+struct DepositParams {
+	IVaultStrategy strategy;
+	VaultType vaultType;
+	uint256 amountIn;
+	uint256 minSharesOut;
+}
+
+// Sector Aggregator Vault
+contract AggregatorEpochVault is SectorBaseEpoch {
+	using FixedPointMathLib for uint256;
+	using SafeERC20 for ERC20;
+
+	/// if vaults accepts native asset we set asset to address 0;
+	address internal constant NATIVE = address(0);
+
+	// resonable amount to not go over gas limit when doing emergencyWithdraw
+	// in reality can go up to 200
+	uint8 MAX_STRATS = 100;
+
+	mapping(IVaultStrategy => bool) public strategyExists;
+	address[] public strategyIndex;
+
+	uint256 public totalStrategyHoldings;
+
+	constructor(
+		ERC20 asset_,
+		string memory _name,
+		string memory _symbol,
+		bool _useNativeAsset,
+		uint256 _maxTvl,
+		AuthConfig memory authConfig,
+		FeeConfig memory feeConfig
+	)
+		ERC4626(asset_, _name, _symbol, _useNativeAsset)
+		Auth(authConfig)
+		Fees(feeConfig)
+		SectorBaseEpoch()
+	{
+		maxTvl = _maxTvl;
+		emit MaxTvlUpdated(_maxTvl);
+	}
+
+	function addStrategy(IVaultStrategy strategy) public onlyOwner {
+		if (strategyIndex.length >= MAX_STRATS) revert TooManyStrategies();
+		if (strategyExists[strategy]) revert StrategyExists();
+
+		/// make sure underlying matches
+		if (address(strategy.underlying()) != address(asset)) revert WrongUnderlying();
+
+		strategyExists[strategy] = true;
+		strategyIndex.push(address(strategy));
+		emit AddStrategy(address(strategy));
+	}
+
+	function removeStrategy(IVaultStrategy strategy) public onlyOwner {
+		if (!strategyExists[strategy]) revert StrategyNotFound();
+		strategyExists[strategy] = false;
+		uint256 length = strategyIndex.length;
+		// replace current index with last strategy and pop the index array
+		uint256 i;
+		for (i; i < length; ++i) if (address(strategy) == strategyIndex[i]) break;
+		strategyIndex[i] = strategyIndex[length - 1];
+		strategyIndex.pop();
+		emit RemoveStrategy(address(strategy));
+	}
+
+	function totalStrategies() external view returns (uint256) {
+		return strategyIndex.length;
+	}
+
+	function getAllStrategies() external view returns (address[] memory) {
+		return strategyIndex;
+	}
+
+	/// We compute expected tvl off-chain first, to ensure this transactions isn't sandwitched
+	function harvest(uint256 expectedTvl, uint256 maxDelta) public onlyRole(MANAGER) {
+		uint256 currentChildHoldings = _getStrategyHoldings();
+		uint256 tvl = currentChildHoldings + floatAmnt;
+		_checkSlippage(expectedTvl, tvl, maxDelta);
+		// harvest event emitted here
+		_harvest(currentChildHoldings);
+	}
+
+	/// this can be done in parts in case gas limit is reached
+	function depositIntoStrategies(DepositParams[] calldata params) public onlyRole(MANAGER) {
+		uint256 l = params.length;
+		for (uint256 i; i < l; ++i) {
+			DepositParams memory param = params[i];
+			uint256 amountIn = param.amountIn;
+			if (amountIn == 0) continue;
+			IVaultStrategy strategy = param.strategy;
+
+			if (!strategyExists[strategy]) revert StrategyNotFound();
+
+			// update underlying float accouting
+			beforeWithdraw(amountIn, 0);
+			/// push funds to avoid approvals
+			if (param.vaultType == VaultType.Strategy) {
+				// send funds - more gas efficient
+				if (strategy.sendERC20ToStrategy() == true)
+					asset.safeTransfer(strategy.strategy(), amountIn);
+				else asset.safeTransfer(address(strategy), amountIn);
+
+				// process deposit
+				strategy.deposit(address(this), address(asset), 0, param.minSharesOut);
+			} else if (param.vaultType == VaultType.Aggregator) {
+				asset.safeApprove(address(strategy), amountIn);
+				strategy.deposit(amountIn, address(this));
+			}
+			totalChildHoldings += amountIn;
+
+			emit DepositIntoStrategy(msg.sender, address(strategy), amountIn);
+		}
+	}
+
+	function requestRedeemFromStrategies(RedeemParams[] calldata params) public onlyRole(MANAGER) {
+		uint256 l = params.length;
+		for (uint256 i; i < l; ++i) {
+			if (params[i].vaultType != VaultType.Aggregator) continue;
+			params[i].strategy.requestRedeem(params[i].shares);
+		}
+	}
+
+	/// this can be done in parts in case gas limit is reached
+	function withdrawFromStrategies(RedeemParams[] calldata params) public onlyRole(MANAGER) {
+		uint256 l = params.length;
+		for (uint256 i; i < l; ++i) {
+			RedeemParams memory param = params[i];
+			uint256 shares = param.shares;
+			if (shares == 0) continue;
+			IVaultStrategy strategy = param.strategy;
+			if (!strategyExists[strategy]) revert StrategyNotFound();
+
+			uint256 amountOut;
+			if (param.vaultType == VaultType.Strategy)
+				// no need to push share tokens - contract can burn them
+				amountOut = strategy.redeem(
+					address(this),
+					shares,
+					address(asset), // token out is allways asset
+					param.minTokenOut
+				);
+			else if (param.vaultType == VaultType.Aggregator) amountOut = strategy.redeem();
+
+			// if strategy was profitable, we may end up withdrawing more than totalChildHoldings
+			totalChildHoldings = amountOut > totalChildHoldings
+				? 0
+				: totalChildHoldings - amountOut;
+
+			// update underlying float accounting
+			afterDeposit(amountOut, 0);
+			emit WithdrawFromStrategy(msg.sender, address(strategy), amountOut);
+		}
+	}
+
+	/// gets accurate strategy holdings denominated in asset
+	function _getStrategyHoldings() internal returns (uint256 tvl) {
+		uint256 l = strategyIndex.length;
+		/// TODO compute realistic limit for strategy array lengh to stay within gas limit
+		for (uint256 i; i < l; ++i) {
+			IVaultStrategy strategy = IVaultStrategy(payable(strategyIndex[i]));
+			tvl += strategy.getUpdatedUnderlyingBalance(address(this));
+		}
+	}
+
+	/// returns expected tvl (used for estimate)
+	function getTvl() public view returns (uint256 tvl) {
+		uint256 l = strategyIndex.length;
+		// there should be no untrusted strategies in this array
+		for (uint256 i; i < l; ++i) {
+			IVaultStrategy strategy = IVaultStrategy(payable(strategyIndex[i]));
+			tvl += strategy.underlyingBalance(address(this));
+		}
+		tvl += asset.balanceOf(address(this));
+	}
+
+	function totalAssets() public view virtual override returns (uint256) {
+		return floatAmnt + totalChildHoldings;
+	}
+
+	/// INTERFACE UTILS
+
+	/// @dev returns accurate value used to estimate current value
+	function estimateUnderlyingBalance(address user) external view returns (uint256) {
+		uint256 shares = balanceOf(user);
+		// value based on last harvest exchange rate
+		uint256 cachedValue = convertToAssets(shares);
+		// valued based on current tvl
+		uint256 currentValue = sharesToUnderlying(shares);
+		return cachedValue > currentValue ? currentValue : cachedValue;
+	}
+
+	/// @dev current exchange rate (different from previewDeposit rate)
+	/// this should be used for estiamtes of withdrawals
+	function sharesToUnderlying(uint256 shares) public view returns (uint256) {
+		uint256 supply = totalSupply(); // Saves an extra SLOAD if totalSupply is non-zero.
+		return supply == 0 ? shares : shares.mulDivDown(getTvl(), supply);
+	}
+
+	/// @dev current exchange rate (different from previewDeposit / previewWithdrawal rate)
+	/// this should be used estimate of deposit fee
+	function underlyingToShares(uint256 underlyingAmnt) public view returns (uint256) {
+		uint256 supply = totalSupply(); // Saves an extra SLOAD if totalSupply is non-zero.
+		return supply == 0 ? underlyingAmnt : underlyingAmnt.mulDivDown(supply, getTvl());
+	}
+
+	error TooManyStrategies();
+	error VaultAddressNotMatch();
+	event AddStrategy(address indexed strategy);
+	event RemoveStrategy(address indexed strategy);
+	event DepositIntoStrategy(address caller, address strategy, uint256 amount);
+	event WithdrawFromStrategy(address caller, address strategy, uint256 amount);
+}
