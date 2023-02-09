4d41badca8d37c8ee8f4c7f1ec2ded69822007df
0x0scion <scionfinance@protonmail.com>
2023-02-09 13:34:46 -0500

    checksummed addr

--- ts/config/utils.ts
+++ ts/config/utils.ts
@@ -23,7 +23,7 @@ export const tokens = {
   },
   arbitrum: {
     USDC: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-    ETH: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+    ETH: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
     XCAL: '0xd2568acCD10A4C98e87c44E9920360031ad89fCB',
   },
 };
